9bb218380ac3f6f8b8c2d71e66d1acee11a0de7f
Semen <semyanyshyn@gmail.com>
2026-03-29 05:51:02 +0300

    completed item №6 of 17 Lesson

--- 01.js
+++ 01.js
@@ -35,9 +35,12 @@ async function getPlanets() {
 
     for (let i = 0; i < dataResult.length; i++) {
       const namesOfPlanets = dataResult[i].name;
+      const planetId = dataResult[i].uid;
       const p = document.createElement("p");
       p.append(namesOfPlanets);
       planet.append(p);
+      p.id = planetId;
+      p.classList.add("planet");
     }
   } catch (error) {
     planet.textContent = "Opps! No data found.";
@@ -48,3 +51,26 @@ async function getPlanets() {
 }
 
 btn.addEventListener("click", getPlanets);
+
+const modalBody = document.getElementById("modalBody");
+const modalContainer = document.getElementById("planetModal");
+
+document.body.addEventListener("click", (event) => {
+  const pushedElem = event.target.id;
+  if (event.target.classList.contains("planet")) {
+    async function getInfo() {
+      const dataPlanet = await fetch(
+        `https://swapi.tech/api/planets/${pushedElem}`,
+      );
+      const dataPlanetJson = await dataPlanet.json();
+      console.log(dataPlanetJson);
+      const props = dataPlanetJson.result.properties;
+      modalBody.innerHTML = `<pre>${JSON.stringify(props, null, 2)}</pre>`;
+      const modal = new bootstrap.Modal(modalContainer);
+      //щоб відкрити Bootstrap modal, треба не просто HTML-елемент,
+      // а Bootstrap-об’єкт модалки.
+      modal.show();
+    }
+    getInfo();
+  }
+});
